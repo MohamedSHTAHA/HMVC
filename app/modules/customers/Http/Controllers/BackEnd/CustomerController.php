aa83625c80cd5dcf07c3e9649b967a6beb8e9115
MohamedSHTAHA <mohamed.sh.taha2015@gmail.com>
2022-01-02 00:07:00 +0200

    hmvc example

---
+++ app/modules/customers/Http/Controllers/BackEnd/CustomerController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Customers\Http\Controllers\BackEnd;
+
+use App\Http\Controllers\Controller;
+
+class CustomerController extends Controller{
+    public function index( )
+    {
+        dd('taha');
+    }
+}
