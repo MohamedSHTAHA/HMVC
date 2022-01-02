043e781059c70d0e10b46ae5e7b75a189c577cdc
MohamedSHTAHA <mohamed.sh.taha2015@gmail.com>
2022-01-03 01:29:07 +0200

    request & middleware

--- app/modules/customers/Http/Controllers/FrontEnd/CustomerController.php
+++ app/modules/customers/Http/Controllers/FrontEnd/CustomerController.php
@@ -3,9 +3,10 @@
 namespace Customers\Http\Controllers\FrontEnd;
 
 use App\Http\Controllers\Controller;
+use Customers\Http\Requests\CustomerRequest;
 
 class CustomerController extends Controller{
-    public function index( )
+    public function index(CustomerRequest $requst )
     {
        return view('customers::frontend.index');
     }
--- app/modules/customers/routes/web.php
+++ app/modules/customers/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('')->group(function () {
     Route::namespace('BackEnd')->prefix(config('customers.prefix.backend', config('module.prefix.backend')))->group(function () {
         Route::prefix(config('customers.prefix.module-name'))->group(function () {
-            Route::get('home', 'CustomerController@index');
+            Route::get('home', 'CustomerController@index')->middleware('customer_middleware');
         });
     });
     Route::namespace('FrontEnd')->prefix(config('customers.prefix.frontend', config('module.prefix.frontend')))->group(function () {
